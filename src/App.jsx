b8c7ac596a65599d91c0deb5b9198d9551a2fe04
Natarajan Gothandaraman <165759111+Natarajan-Gothandaraman@users.noreply.github.com>
2025-09-10 09:29:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
           <div className="flex items-center space-x-3 mr-20">
             <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-cyan-500">
               <img
-                src="/assets/userimage.jpg"
+                src="https://res.cloudinary.com/dybicylgj/image/upload/v1757476632/userimage_ncyiht.jpg"
                 alt="Profile"
                 className="w-full h-full object-cover"
               />
